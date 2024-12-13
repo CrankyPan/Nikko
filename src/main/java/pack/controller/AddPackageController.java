8262554d07e81f9a614e23a30a7cfb21645060c2
MUHAMMAD YAZID <95423355+CrankyPan@users.noreply.github.com>
2024-12-13 22:48:05 +0800

    Update AddPackageController.java

--- src/main/java/pack/controller/AddPackageController.java
+++ src/main/java/pack/controller/AddPackageController.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 
 /**
